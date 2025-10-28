20d581afbc3f0acfc7231eeb4e9b95c3f401c234
isma159 <113428837+isma159@users.noreply.github.com>
2025-10-28 11:03:37 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.wordsearch {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.wordsearch to javafx.fxml;
-    exports com.example.wordsearch;
-}
